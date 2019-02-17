fea13bd9c8344326ffa707633779d31e816a7bb2
Caiomyra <caiomyrapereira@hotmail.com>
2019-02-17 15:37:22 -0300

    Create exercicio01.rb

---
+++ 30exercicios/exercicio01.rb
@@ -0,0 +1,10 @@
+=begin
+1) Efetuar o cálculo do salário líquido de um trabalhador baseando-se na estrutura abaixo:
+Salário Bruto Descontos Salário Líquido
+<=1000, 3%, ?
+<=2000 5%, ?
+<=3000 7,5%, ?
+<=5000 10%, ?
+>5000 12,5%, ?
+O sistema deverá informar os valores de desconto e salario_liquido a partir do salario_bruto fornecido pelo usuário.
+=end
