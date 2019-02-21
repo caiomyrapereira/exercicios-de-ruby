bb839dbba8d66581438a41a4b032b704413fd4e3
Caiomyra <caiomyrapereira@hotmail.com>
2019-02-21 12:53:33 -0300

    Create exercicio05.rb

---
+++ 30exercicios/exercicio05.rb
@@ -0,0 +1,7 @@
+#Ler um número real e imprimir a terça parte deste número.
+
+puts "digite um numero?";
+numero = gets.to_f;
+resultado = numero/3;
+puts "terça parte deste número é #{resultado}";
+
