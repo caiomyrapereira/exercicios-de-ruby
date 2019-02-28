1ae5c7563ef5a15f89e8e5064c0dbbf46a0ee343
Caiomyra <caiomyrapereira@hotmail.com>
2019-02-27 23:14:45 -0300

    Create exercicio.rb

---
+++ 30exercicios/exercicio.rb
@@ -0,0 +1,6 @@
+#Entrar com um número e imprimir o logaritmo desse número na base 10
+puts "digite um numero??";
+numero=gets.to_f;
+
+resultado_base10 = Math.log10(numero);
+puts "resultado de logaritmo na base10:#{resultado_base10}";
