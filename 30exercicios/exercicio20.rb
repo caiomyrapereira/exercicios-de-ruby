b40726856870bf2b9b982166643e2a0621136363
Caiomyra <caiomyrapereira@hotmail.com>
2019-03-19 01:33:56 -0300

    Update exercicio20.rb

--- 30exercicios/exercicio20.rb
+++ 30exercicios/exercicio20.rb
@@ -1,4 +1,5 @@
-
+#Faça um algoritmo que leia um valor de conta de restaurante, representando o gasto realizado pelo cliente e
+#imprima o valor total a ser pago, considerando que o restaurante cobra 10% para o garçom.
 puts "digite o valor conta de restaurante?"
 conta = gets.to_f;
 total = (conta*1.1).to_i;
